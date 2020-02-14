41fba13d0e4e067ab5a66cb48a567eed224f3278
Santiago Arboleda L <monoprosito@gmail.com>
2020-02-14 17:13:33 -0500

    Re-create an instance with a dict representation

--- models/base_model.py
+++ models/base_model.py
@@ -34,16 +34,24 @@ class BaseModel:
 
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Base Model __init__ Method
 
         Here, the default values of a Base Model
         instance are initialized.
 
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for arg, val in kwargs.items():
+                if arg in ('created_at', 'updated_at'):
+                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+
+                if arg != '__class__':
+                    setattr(self, arg, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Representation of the class for the user
