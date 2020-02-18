7ec1b102b9fb1fbab997e420524a78199141d3e7
Thorlak2202 <darkdrakkar@gmail.com>
2020-02-17 20:52:36 -0500

    the console will be controlling all changes made to the databases, this commit adds the create feature and all evaluations required for the feature

--- console.py
+++ console.py
@@ -1,24 +1,53 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Console Module
+This module controls all databases.
+Can create, modify and delete instances.
 """
-"""
+
+
+from datetime import datetime
 import cmd
+import models
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """command processor class."""
     prompt = '(hbnb) '
+    allowed_classes = ['BaseModel', 'User', 'State', 'City',
+                       'Amenity', 'Place', 'Review']
 
     def do_quit(self, line):
-        """Quit command to exit the program
+        """Quit command to exit the program.
         """
         return True
 
     def do_EOF(self, line):
-        """Quit command to exit the program
+        """Quit command to exit the program.
         """
         return True
 
+    def do_create(self, line):
+        """Creates a new instance of BaseModel.
+        """
+        command = self.parseline(line)[0]
+        if command is None:
+            print('** class name missing **')
+        elif command not in self.allowed_classes:
+            print("** class doesn't exist **")
+        else:
+            new_obj = eval(command)()
+            new_obj.save()
+            print(new_obj.id)
+
     def emptyline(self):
+        """Emptyline Documentation
+        Method called when an empty line is entered in
+        response to the prompt. If this method is not
+        overridden, it repeats the last nonempty
+        command entered.
+        """
         pass
 
 
