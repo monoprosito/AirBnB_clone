8175ed6406a0b80f7bf4e984c59c61a82d7a536b
Christian David Suárez Guerra <darkdrakkar@gmail.com>
2020-02-18 17:09:54 -0500

    changed hierarchy of files in test folder

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """File Storage Module
-This module is in charge of ...
+This module is in charge of the storage of the
+classes and their management.
 """
 
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
 from os import path
 
 
 class FileStorage:
     """File Storage Class
-    This is the File Storage ...
+    This is the File Storage module.
     Attributes:
         __file_path (str): This is the path of the JSON file in which
             the contents of the `__objects` variable will be stored.
@@ -28,7 +29,7 @@ class FileStorage:
 
     def all(self):
         """Gets the __objects info
-        Returns the content of the `__objects` class attribute
+        Returns the content of the `__objects` class attribute.
         """
         return self.__objects
 
@@ -37,7 +38,7 @@ class FileStorage:
         Args:
             obj (inst): The object to adds in the `__objects` class attribute
         Sets in the `__objects` class attribute the instance data
-        with a key as <obj class name>.id
+        with a key as <obj class name>.id.
         """
         key = obj.__class__.__name__ + '.' + obj.id
         self.__objects[key] = obj
